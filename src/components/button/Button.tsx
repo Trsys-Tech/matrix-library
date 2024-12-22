ea7df299b68defea5dff9d4af4542fde34c37c1c
Ako <a.veysinejad@gmail.com>
2024-12-22 19:50:51 +0330

    fix: fixed some styles in buttons

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -6,22 +6,22 @@ import { cn } from "../../lib/utils";
 import { SpinnerIcon } from "../Icons/SpinnerIcon";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-sm text-xs font-normal transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:bg-muted disabled:text-gray-500",
+  "m-1 inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-sm text-xs font-normal transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:bg-muted disabled:text-gray-500",
   {
     variants: {
       variant: {
         primary:
-          "bg-primary text-primary-foreground hover:bg-primary-600 focus:bg-primary focus:border-primary focus:ring focus:ring-primary-300 active:bg-primary active:ring active:ring-1 active:ring-primary active:ring-offset-2",
+          "bg-primary text-primary-foreground hover:bg-primary-600 focus:border-primary focus:ring focus:ring-primary-300 active:bg-primary active:ring active:ring-1 active:ring-primary active:ring-offset-2",
         outline:
           "border border-primary text-primary hover:bg-primary-50 focus:bg-primary-50 focus:border-primary-100 focus:ring focus:ring-primary-100 active:bg-primary-50 active:border-primary active:ring-1 active:ring-primary active:ring-offset-2 disabled:border-gray-400",
-        text: "text-text [&>svg]:text-primary border border-transparent hover:bg-primary-50 focus:border-primary-300 focus:ring-primary-300 focus:ring-2 active:border-primary disabled:text-text-300 [&>svg]:disabled:text-text-300 disabled:bg-transparent",
+        text: "text-text [&>svg]:text-primary border border-transparent hover:bg-primary-50 focus:ring focus:ring-primary-100 active:ring-1 active:ring-primary disabled:text-text-300 [&>svg]:disabled:text-text-300 disabled:bg-transparent",
         danger:
-          "bg-danger text-danger-foreground hover:bg-danger-600 focus:bg-danger focus:ring focus:ring-danger-400 active:bg-danger active:ring-1 active:ring-danger active:ring-offset-2",
+          "bg-danger text-danger-foreground hover:bg-danger-600 focus:ring focus:ring-danger-400 active:bg-danger active:ring-1 active:ring-danger active:ring-offset-2",
         warning:
-          "bg-warning text-warning-foreground hover:bg-warning-600 focus:bg-warning focus:ring focus:ring-warning-400 active:bg-warning active:ring-1 active:ring-warning active:ring-offset-2",
+          "bg-warning text-warning-foreground hover:bg-warning-600 focus:ring focus:ring-warning-400 active:bg-warning active:ring-1 active:ring-warning active:ring-offset-2",
         success:
-          "bg-success text-success-foreground hover:bg-success-600 focus:bg-success focus:ring focus:ring-success-400 active:bg-success active:ring-1 active:ring-success active:ring-offset-2",
-        info: "bg-info text-info-foreground hover:bg-info-600 focus:bg-info focus:ring focus:ring-info-400 active:bg-info active:ring-1 active:ring-info active:ring-offset-2",
+          "bg-success text-success-foreground hover:bg-success-600 focus:ring focus:ring-success-400 active:bg-success active:ring-1 active:ring-success active:ring-offset-2",
+        info: "bg-info text-info-foreground hover:bg-info-600 focus:ring focus:ring-info-400 active:bg-info active:ring-1 active:ring-info active:ring-offset-2",
         primaryOnDark:
           "bg-gray-0 text-primary hover:bg-primary-50 focus:bg-gray-0 focus:border-none focus:ring focus:ring-primary-100 active:ring-0 active:bg-gray-0 active:outline active:outline-2 active:outline-gray-0 active:outline-offset-1",
         outlineOnDark:
