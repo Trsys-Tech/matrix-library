b770a579b85c379bc56f45029eddeb6ec96c2558
Ako <a.veysinejad@gmail.com>
2026-03-30 14:58:26 +0330

    fix: fixed tailwind generated css variable names still used --tw

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,31 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
+const TAILWIND_VAR_PREFIX = /--tw-/g;
+
+function renameTailwindCssVariables() {
+  return {
+    postcssPlugin: "rename-tailwind-css-variables",
+    OnceExit(root) {
+      root.walkDecls((decl) => {
+        if (decl.prop.includes("--tw-")) {
+          decl.prop = decl.prop.replace(TAILWIND_VAR_PREFIX, "--mtx-");
+        }
+
+        if (decl.value.includes("--tw-")) {
+          decl.value = decl.value.replace(TAILWIND_VAR_PREFIX, "--mtx-");
+        }
+      });
+
+      root.walkAtRules((atRule) => {
+        if (atRule.params.includes("--tw-")) {
+          atRule.params = atRule.params.replace(TAILWIND_VAR_PREFIX, "--mtx-");
+        }
+      });
+    },
+  };
 }
+
+export default {
+  plugins: [tailwindcss(), autoprefixer(), renameTailwindCssVariables()],
+};
